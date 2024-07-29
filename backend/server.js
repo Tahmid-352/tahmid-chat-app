8c26e90c731be7854de3439ad9ea6d33140e96a0
Tahmid-352 <tahmidkhan017433@gmail.com>
2024-07-29 19:10:59 +0600

    deployment

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,14 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
+dotenv.config();
 
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with jason payloads (from req.body)
 app.use(cookieParser());
@@ -19,6 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
